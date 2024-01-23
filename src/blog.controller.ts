0b8568cfb7738646916d269b973b06160a178dc1
BaeJia <qowldk1030@gmail.com>
2024-01-23 19:50:49 +0900

    feat : 메모리에 데이터를 저장하는 api

--- src/blog.controller.ts
+++ src/blog.controller.ts
@@ -7,33 +7,45 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { BlogService } from './blog.service';
 
 @Controller('blog')
 export class BlogController {
+  blogService: BlogService;
+
+  constructor() {
+    this.blogService = new BlogService();
+  }
+
   @Get()
   getAllPosts() {
     console.log('모든 게시글 가져오기');
+    return this.blogService.getAllPosts();
   }
 
   @Post()
-  createPost(@Body() post: any) {
+  createPost(@Body() postDto) {
     console.log('게시글 작성');
-    console.log(post);
+    this.blogService.createPost(postDto);
+    return 'success';
   }
 
   @Get('/:id')
   getPost(@Param('id') id: string) {
-    console.log(`[id: ${id}]게시글 하나 가져오기`);
+    console.log('게시글 하나 가져오기');
+    return this.blogService.getPost(id);
   }
 
   @Delete('/:id')
-  deletePost() {
+  deletePost(@Param('id') id: string) {
     console.log('게시글 삭제');
+    this.blogService.delete(id);
+    return 'success';
   }
 
   @Put('/:id')
-  updatePost(@Param('id') id, @Body() post: any) {
-    console.log(`[${id}] 게시글 업데이트`);
-    console.log(post);
+  updatePost(@Param('id') id: string, @Body() postDto) {
+    console.log('게시글 업데이트', id, postDto);
+    return this.blogService.updatePost(id, postDto);
   }
 }
